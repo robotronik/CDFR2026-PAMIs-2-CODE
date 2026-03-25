f5e6f74a5eabf10d589aecdcdda469169f3f8bc3
Robin <robin.pierson@grenoble-inp.org>
2026-03-25 15:00:39 +0100

    Feat: AGGRESSIVE error checking

--- main/src/actuators/leds.cpp
+++ main/src/actuators/leds.cpp
@@ -8,7 +8,7 @@ Led::Led(gpio_num_t pin) : pin(pin) {
         .pull_down_en = GPIO_PULLDOWN_DISABLE, 
         .intr_type = GPIO_INTR_DISABLE
     };
-    gpio_config(&default_pull_config);
+    ESP_ERROR_CHECK(gpio_config(&default_pull_config));
 }
 
 gpio_num_t Led::get_pin() {
@@ -16,6 +16,6 @@ gpio_num_t Led::get_pin() {
 }
 
 void StatusLed::toggle() {
-    gpio_set_level(get_pin(), !current_state);
+    ESP_ERROR_CHECK(gpio_set_level(get_pin(), !current_state));
     current_state = !current_state;
 }
--- main/src/locomotion/motor.cpp
+++ main/src/locomotion/motor.cpp
@@ -10,50 +10,50 @@ Motor::Motor(gpio_num_t pin_a, gpio_num_t pin_b) {
     timer_config.count_mode = MCPWM_TIMER_COUNT_MODE_UP;
     timer_config.period_ticks = 1000; 
     
-    mcpwm_new_timer(&timer_config, &timer);
+    ESP_ERROR_CHECK(mcpwm_new_timer(&timer_config, &timer));
  
     /* Operator setup */
     mcpwm_operator_config_t oper_config = {};
     oper_config.group_id = 0;
-    mcpwm_new_operator(&oper_config, &oper);
-    mcpwm_operator_connect_timer(oper, timer);
+    ESP_ERROR_CHECK(mcpwm_new_operator(&oper_config, &oper));
+    ESP_ERROR_CHECK(mcpwm_operator_connect_timer(oper, timer));
 
     /* Comparator setup */
     mcpwm_comparator_config_t cmpr_config = {};
     cmpr_config.flags.update_cmp_on_tez = true;
     
-    mcpwm_new_comparator(oper, &cmpr_config, &cmpr1);
-    mcpwm_new_comparator(oper, &cmpr_config, &cmpr2);
+    ESP_ERROR_CHECK(mcpwm_new_comparator(oper, &cmpr_config, &cmpr1));
+    ESP_ERROR_CHECK(mcpwm_new_comparator(oper, &cmpr_config, &cmpr2));
 
     /* Generator setup */
     mcpwm_generator_config_t gen1_config = {};
     gen1_config.gen_gpio_num = pin_a;
-    mcpwm_new_generator(oper, &gen1_config, &gen1);
+    ESP_ERROR_CHECK(mcpwm_new_generator(oper, &gen1_config, &gen1));
     
     mcpwm_generator_config_t gen2_config = {};
     gen2_config.gen_gpio_num = pin_b;
-    mcpwm_new_generator(oper, &gen2_config, &gen2);
+    ESP_ERROR_CHECK(mcpwm_new_generator(oper, &gen2_config, &gen2));
 
     /* Enable timer */
-    mcpwm_generator_set_action_on_timer_event(gen1, 
-        MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH));
-    mcpwm_generator_set_action_on_timer_event(gen2, 
-        MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH));
+    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(gen1, 
+        MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH)));
+    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(gen2, 
+        MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH)));
         
-    mcpwm_generator_set_action_on_compare_event(gen1, 
-        MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, cmpr1, MCPWM_GEN_ACTION_LOW));
-    mcpwm_generator_set_action_on_compare_event(gen2, 
-        MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, cmpr2, MCPWM_GEN_ACTION_LOW));
+    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(gen1, 
+        MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, cmpr1, MCPWM_GEN_ACTION_LOW)));
+    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(gen2, 
+        MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, cmpr2, MCPWM_GEN_ACTION_LOW)));
 }
 
 void Motor::start() {
-    mcpwm_timer_enable(timer);
-    mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP);
+    ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
+    ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP));
 }
 
 void Motor::stop() {
-    mcpwm_timer_start_stop(timer, MCPWM_TIMER_STOP_EMPTY);
-    mcpwm_timer_disable(timer);
+    ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_STOP_EMPTY));
+    ESP_ERROR_CHECK(mcpwm_timer_disable(timer));
 }
 
     
