357c049a9b0e0493d7dcf203f8ed02b031424147
Ludovic Bouchard <ludovicb1239@gmail.com>
2026-04-20 20:53:06 +0200

    fix: correct pull switch GPIO configuration and read logic

--- main/src/actuators/pull_switch.cpp
+++ main/src/actuators/pull_switch.cpp
@@ -7,13 +7,13 @@ PullSwitch::PullSwitch(gpio_num_t pin) : pin(pin) {
     gpio_config_t default_pull_config = {
         .pin_bit_mask = (1ULL << pin),
         .mode = GPIO_MODE_INPUT,
-        .pull_up_en = GPIO_PULLUP_DISABLE,
-        .pull_down_en = GPIO_PULLDOWN_ENABLE, 
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .intr_type = GPIO_INTR_DISABLE
     };
     ESP_ERROR_CHECK(gpio_config(&default_pull_config));
 }
 
 int PullSwitch::read() {
-   return gpio_get_level(PIN_SW_TIRETTE) ? 1 : 0;
+    return gpio_get_level(PIN_SW_TIRETTE) ? 0 : 1;
 }
