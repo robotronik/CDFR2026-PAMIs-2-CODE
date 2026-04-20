48a6fda56458499d9699682cc91c3cd33f7d5b22
Robin <robin.pierson@grenoble-inp.org>
2026-04-20 14:38:22 +0200

    Feat: map find closest object

--- main/src/navigation/map.cpp
+++ main/src/navigation/map.cpp
@@ -1,10 +1,7 @@
 #include "navigation/map.h"
 #include "structs.h"
 #include "constants.h"
-
-Map::Map() {
-       
-}
+#include "math.h"
 
 void Map::add_object(map_object_t object) {
     objects.push_back(object);
@@ -16,8 +13,16 @@ void Map::remove_object(map_object_t object) {
 
 map_object_t Map::find_closest_object() {
     map_object_t result;
+    float lowest_distance = 100000.0f;
     for(const auto& object: objects) {
-        
+        coords_t current_pos = shared_pos.get();
+        float delta_x = current_pos.x - object.position.x;
+        float delta_y = current_pos.y - object.position.y;
+        float distance = sqrt(delta_x * delta_x + delta_y * delta_y);
+        if(distance < lowest_distance) {
+            lowest_distance = distance;
+            result = object;
+        }
     }
     return result;
 }
