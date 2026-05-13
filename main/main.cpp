0b2e27d96b9652b45f7d802c85b39924925e25bd
Robin <robin.pierson@grenoble-inp.org>
2026-05-13 22:39:02 +0200

    fix

--- main/main.cpp
+++ main/main.cpp
@@ -14,7 +14,6 @@
     #include "wireless/web_server.h"
 #endif
 
-│ └╴  main.cpp
 static const char* LOGGER_TAG = "MainFSM";
 static MainFSMState current_state = MainFSMState::INIT;
 Team current_team = Team::BLUE;
